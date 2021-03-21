bd917322f5bfb9d1ca3f5eea29b2df806cfc8604
Petreti Andrea <petretiandrea@gmail.com>
2021-03-21 17:13:43 +0100

    update

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,39 +1,43 @@
 <template>
   <v-app-bar fixed elevate-on-scroll ref="appbar" :color="appBarColor" dark>
-    <v-layout justify-center>
       <v-container>
         <v-layout>
           <div>
             <v-btn text>Andrea Petreti</v-btn>
           </div>
           <v-spacer></v-spacer>
-          <v-toolbar-items class="text-right">
-            <v-btn
-              :x-small="$vuetify.breakpoint.smAndDown"
-              @click="$vuetify.goTo('#section-about-me',options )"
-              text
-            >About Me</v-btn>
-            <v-btn
-              :x-small="$vuetify.breakpoint.smAndDown"
-              @click="$vuetify.goTo('#portfolio',options )"
-              text
-            >My Work</v-btn>
-            <v-btn
-              :x-small="$vuetify.breakpoint.smAndDown"
-              @click="$vuetify.goTo('#contact',options )"
-              text
-            >Contact</v-btn>
+
+          <v-toolbar-items class="text-right hidden-sm-and-down">
+            <template v-for="item in menu">
+              <v-btn :key="item.goto_id"
+                :x-small="$vuetify.breakpoint.smAndDown"
+                @click="$vuetify.goTo(item.goto_id,options)" 
+                text> {{ item.section_name }}</v-btn>
+            </template>
           </v-toolbar-items>
+
+          <v-menu class="hidden-md-and-up">
+            <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+            <v-list>
+              <v-list-tile v-for="item in menu" :key="item.goto_id">
+                <v-list-tile-content><v-list-tile-title>{{ item.section_name }}</v-list-tile-title></v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-menu>
         </v-layout>
       </v-container>
-    </v-layout>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   data: () => ({
-    appBarScrolled: false
+    appBarScrolled: false,
+    menu: [
+      { section_name: "About Me", goto_id: "#section-about-me"},
+      { section_name: "My Work", goto_id: "#portfolio"},
+      { section_name: "Contact", goto_id: "#contact"}
+    ]
   }),
   computed: {
     appBarColor: function() {
