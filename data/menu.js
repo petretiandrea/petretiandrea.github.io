51ba52899344f6236d4870dfcf087582d5c14be6
petretiandrea <petretiandrea@gmail.com>
2024-12-11 23:09:56 +0100

    disable curriculum section

--- data/menu.js
+++ data/menu.js
@@ -1,8 +1,7 @@
 const menu = [
     { name_key: "menu_home", path: "/" },
     { name_key: "menu_projects", path: "/projects" },
-    { name_key: "menu_blog", path: "/blog" },
-    { name_key: "menu_cv", path: "/cv/index.html", external: true }
+    { name_key: "menu_blog", path: "/blog" }
   ];
   
   export default menu;
